95e57a0ea8ce82a752c58819da9ae2e0849b6694
Thanqnv <23020707@vnu.edu.vn>
2024-03-14 23:53:12 +0700

    commit guessIt

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -70,7 +70,7 @@ string getAnswer(int number, int randomNumber) {
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
     
-    if (answer != "Congutulation! You win.") return false;
+    if (answer != "Congratulation! You win.") return false;
     else return true;
 
     return true;
